677148c2fd35416983e85d3aac77ee1b367aa735
EliNygard <eli.nygard@gmail.com>
2025-03-08 17:27:47 +0100

    adds message if cart is empty

--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -1,14 +1,31 @@
+import { RootState } from "@/app/store";
+import Button from "@/components/Button";
 import Checkout from "@/components/Checkout";
 import { Helmet } from "react-helmet-async";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 export const Cart = () => {
+  const cart = useSelector((state: RootState) => state.cart.products);
+
   return (
     <>
       <Helmet>
         <title>Infinite Finds - Cart and checkout</title>
         <meta name="description" content="Cart" />
       </Helmet>
-      <Checkout />
+      {cart.length === 0 ? (
+        <div className="my-4 mx-auto flex flex-col items-center max-w-3xl">
+          <p>Your shopping cart is empty</p>
+          <div className="w-full sm:w-1/3 md:w-1/4 mb-8 my-8">
+            <Link to={"/"}>
+              <Button>Back to shop</Button>
+            </Link>
+          </div>
+        </div>
+      ) : (
+        <Checkout />
+      )}
     </>
   );
 };
