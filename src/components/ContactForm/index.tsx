fd429e8ccfa6ff4a1d74170da1759c1a2efbb586
EliNygard <eli.nygard@gmail.com>
2025-03-11 13:48:03 +0100

    installs zod, react label, adds form and input from shadcn ui, builds contact page

---
+++ src/components/ContactForm/index.tsx
@@ -0,0 +1,91 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import Button from "@/components/Button";
+
+const formSchema = z.object({
+  name: z
+    .string()
+    .min(2, { message: "Must be 2 or more characters long" })
+    .max(50, { message: "Must be 50 or fewer characters long" }),
+  email: z.string().email({ message: "Invalid email address" }),
+  message: z
+    .string()
+    .min(2, { message: "Must be 2 or more characters long" })
+    .max(2000, { message: "Must be 2000 or fewer characters long" }),
+});
+
+export function ContactForm() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      email: "",
+      message: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input placeholder="Your email" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="name"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Name</FormLabel>
+              <FormControl>
+                <Input placeholder="Your name" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="message"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Message</FormLabel>
+              <FormControl>
+                <Input placeholder="Your message" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  );
+}
--- src/pages/Contact/index.tsx
+++ src/pages/Contact/index.tsx
@@ -1,3 +1,32 @@
-export const Contact = () => {
-  return <h1>Contact us</h1>;
+import { ContactForm } from "@/components/ContactForm";
+import SearchBar from "@/components/SearchBar";
+import React from "react";
+import { Helmet } from "react-helmet-async";
+
+interface ContactProps {
+  searchTerm: string;
+  setSearchTerm: React.Dispatch<React.SetStateAction<string>>;
+}
+
+export const Contact: React.FC<ContactProps> = ({
+  searchTerm,
+  setSearchTerm,
+}) => {
+  return (
+    <>
+      <Helmet>
+        <title>Infinite Finds - Contact us</title>
+        <meta name="description" content="Infinite Finds - contact us" />
+      </Helmet>
+
+      <SearchBar searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+
+      <section className="border border-black mt-8 mb-8 p-3">
+        <h1 className="font-montserrat text-lg mb-6">Send us a message</h1>
+        <div className="my-3">
+          <ContactForm />
+        </div>
+      </section>
+    </>
+  );
 };
