45fe3cc6c156c19f625da40ecfad9b39b4f87090
EliNygard <eli.nygard@gmail.com>
2025-03-11 19:53:12 +0100

    adds search bar

--- src/pages/Search/index.tsx
+++ src/pages/Search/index.tsx
@@ -7,8 +7,12 @@ import { useFetch } from "@/hooks/useFetch";
 import { IProduct, SearchProps } from "@/interface";
 import { Helmet } from "react-helmet-async";
 import * as S from "../../components/ProductsList/index.styles";
+import SearchBar from "@/components/SearchBar";
 
-export const Search: React.FC<SearchProps> = ({ searchTerm }) => {
+export const Search: React.FC<SearchProps> = ({
+  searchTerm,
+  setSearchTerm,
+}) => {
   const { data, isLoading, isError } = useFetch<IProduct[]>(baseUrl);
 
   console.log("Search page: ", searchTerm);
@@ -32,33 +36,38 @@ export const Search: React.FC<SearchProps> = ({ searchTerm }) => {
 
   const searchResultNumber = searchResult.length;
 
-  if (searchResult.length === 0) {
-    return (
-      <>
-        <p>Unfortunately we do not have what you are looking for.</p>
-        <p>Try a new search. Perhaps we have something else you might like.</p>
-      </>
-    );
-  }
-
   return (
     <>
       <Helmet>
         <title>Infinite Finds - Search</title>
         <meta name="description" content="Infinite Finds - search" />
       </Helmet>
-      <h2>{`Display ${searchResultNumber} products for "${searchTerm}"`}</h2>
-      <S.Ul>
-        {searchResult.map((product) => (
-          <li key={product.id} className="mb-3">
-            <ErrorBoundary
-              fallback={<p>Could not display the product. Please try again.</p>}
-            >
-              <ProductCard product={product} />
-            </ErrorBoundary>
-          </li>
-        ))}
-      </S.Ul>
+      <SearchBar searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+      {searchResult.length === 0 ? (
+        <>
+          <p>Unfortunately we do not have what you are looking for.</p>
+          <p>
+            Try a new search. Perhaps we have something else you might like.
+          </p>
+        </>
+      ) : (
+        <>
+          <h2>{`Display ${searchResultNumber} products for "${searchTerm}"`}</h2>
+          <S.Ul>
+            {searchResult.map((product) => (
+              <li key={product.id} className="mb-3">
+                <ErrorBoundary
+                  fallback={
+                    <p>Could not display the product. Please try again.</p>
+                  }
+                >
+                  <ProductCard product={product} />
+                </ErrorBoundary>
+              </li>
+            ))}
+          </S.Ul>
+        </>
+      )}
     </>
   );
 };
