1e050ea986653e538bf96b2415f86410db9f5601
EliNygard <eli.nygard@gmail.com>
2025-03-12 13:56:33 +0100

    fix: pass data as prop from App.tsx

--- src/pages/Contact/index.tsx
+++ src/pages/Contact/index.tsx
@@ -1,13 +1,12 @@
 import React from "react";
 import { ContactForm } from "@/components/ContactForm";
 import SearchBar from "@/components/SearchBar";
-import { SearchProps } from "@/interface";
+import { IProduct, SearchProps } from "@/interface";
 import { Helmet } from "react-helmet-async";
 
-export const Contact: React.FC<SearchProps> = ({
-  searchTerm,
-  setSearchTerm,
-}) => {
+export const Contact: React.FC<
+  SearchProps & { data: IProduct[]; isLoading: boolean; isError: boolean }
+> = ({ searchTerm, setSearchTerm, data, isLoading, isError }) => {
   return (
     <>
       <Helmet>
@@ -15,7 +14,13 @@ export const Contact: React.FC<SearchProps> = ({
         <meta name="description" content="Infinite Finds - contact us" />
       </Helmet>
 
-      <SearchBar searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+      <SearchBar
+        searchTerm={searchTerm}
+        setSearchTerm={setSearchTerm}
+        data={data}
+        isLoading={isLoading}
+        isError={isError}
+      />
 
       <section className="border border-black mt-8 mb-8 p-3 sm:max-w-9/12 m-auto">
         <div className="my-3">
