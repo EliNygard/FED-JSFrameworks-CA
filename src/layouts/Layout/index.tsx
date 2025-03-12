e61c77ed480fd9618614c75fae7e42a5d907157a
EliNygard <eli.nygard@gmail.com>
2025-03-12 09:24:37 +0100

    feat: toaster message on add to cart

--- src/layouts/Layout/index.tsx
+++ src/layouts/Layout/index.tsx
@@ -1,6 +1,7 @@
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import { Outlet } from "react-router-dom";
+import { Toaster } from "@/components/ui/sonner";
 
 export function Layout() {
   return (
@@ -10,6 +11,7 @@ export function Layout() {
         <Outlet></Outlet>
       </main>
       <Footer />
+      <Toaster />
     </div>
   );
 }
