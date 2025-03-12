3cfce0c64bcefda4b1825ab45abefd6d248035ba
EliNygard <eli.nygard@gmail.com>
2025-03-12 09:33:34 +0100

    set timeout duration for testing skeleton

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -15,7 +15,7 @@ export function useFetch<T>(url: string) {
         const json = await response.json();
         const fetchedData = json.data;
 
-        await new Promise((resolve) => setTimeout(resolve, 3000));
+        await new Promise((resolve) => setTimeout(resolve, 1000));
         setData(fetchedData);
       } catch (error) {
         console.error(error);
