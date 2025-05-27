89c89a8158902d385f7de5538b12df5746e21a75
EliNygard <eli.nygard@gmail.com>
2025-05-27 17:35:41 +0200

    Adds JSDocs and TS types

--- src/components/cartComponents/CartTotals/index.tsx
+++ src/components/cartComponents/CartTotals/index.tsx
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { JSX } from "react";
 import * as S from "./index.styles";
 import { CartTotalsProps } from "@/interface";
 
+/**
+ * CartTotals component displays the total discount and total price for the shopping cart.
+ *
+ * @param {CartTotalsProps} props - Props object containing totals to display.
+ * @param {number} props.cartTotal - The total price of all items in the cart.
+ * @param {number} props.discountTotal - The total discount amount applied to the cart.
+ * @returns {JSX.Element} A styled container displaying discount and total amounts.
+ */
 const CartTotals: React.FC<CartTotalsProps> = ({
   cartTotal,
   discountTotal,
-}) => {
+}: CartTotalsProps): JSX.Element => {
   return (
     <S.CartTotals>
       <div>
