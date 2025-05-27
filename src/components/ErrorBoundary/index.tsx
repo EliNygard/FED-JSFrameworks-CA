641bcd1f042b3521df1be4b1427c2a0c5a8306ff
EliNygard <eli.nygard@gmail.com>
2025-05-27 18:01:26 +0200

    Removes comments

--- src/components/ErrorBoundary/index.tsx
+++ src/components/ErrorBoundary/index.tsx
@@ -19,26 +19,16 @@ class ErrorBoundary extends React.Component<
   }
 
   static getDerivedStateFromError(error: Error): ErrorBoundaryState {
-    // Update state so the next render will show the fallback UI.
     console.error(error);
 
     return { hasError: true };
   }
 
-  // log the error
   componentDidCatch(error: Error, info: React.ErrorInfo): void {
     console.error("Error caught by ErrorBoundary:", error, info);
   }
 
   render() {
-    // You can render any custom fallback UI
-
-    // if (this.state.hasError) {
-    //   return this.props.fallback;
-    // }
-
-    // return this.props.children;
-
     const { fallback, children } = this.props;
     const { hasError } = this.state;
 
